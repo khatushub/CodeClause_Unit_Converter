088c67d82169b6d87f83cdf55eb71a675ca45ad1
Shubham Sharma <123289305+khatushub@users.noreply.github.com>
2023-03-25 22:59:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const feet=document.getElementById('feet');
+const inch=document.getElementById('inch');
+
+feet.addEventListener('input', function(){
+    let f=this.value;
+    let i=f*12;
+
+    inch.value=i;
+})
+
+inch.addEventListener('input', function(){
+    let i=this.value;
+    let f=i/12;
+
+    feet.value=f;
+})
